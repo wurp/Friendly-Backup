41fad1f979c3825d72564a5d2e63385bb2f07578
Bobby Martin <bobbymartin2@gmail.com>
2021-03-06 09:24:17 -0800

    move installation code to Friendly-Backup

---
+++ install.sh
@@ -0,0 +1,17 @@
+set -e
+
+if [ -z "$1" ]
+then
+	echo "Usage: install <dir in which to install>" 1>&2
+	exit -1
+fi
+
+export destdir=$(readlink -m "$1")
+
+cd $(dirname "$0")
+
+export fbdir=$(readlink -m .)
+
+mkdir -p "$destdir"/bin/var
+cp -r "$fbdir"/dist/generic/* "$destdir"/bin/
+ln -s $(readlink -m "$fbdir"/target/Friendly-Backup-*-jar-with-dependencies.jar) "$destdir"/bin/lib/friendly-backup.jar
